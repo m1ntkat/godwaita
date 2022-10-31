dbee305b6bf8e06675f856a7ca8b907b365e8e08
zehkira <9485872-zehkira@users.noreply.gitlab.com>
2022-10-31 19:17:53 +0100

    handle more edge cases, font colors support

--- godwaita/gtk-colors.gd
+++ godwaita/gtk-colors.gd
@@ -7,31 +7,77 @@ const IMAGES = {
 		'checked', 'checked_disabled', 'radio_checked',
 		'radio_checked_disabled'
 	],
-	'CheckButton': [
-		'on', 'on_disabled'
-	]
+	'CheckButton': ['on', 'on_disabled'],
+	'Tree': ['checked'],
+	'PopupMenu': ['radio_checked', 'checked']
 }
 const BOX_PROPS = {
 	'window_bg_color': [
 		['Panel', 'panel', ['bg_color']],
 		['TabContainer', 'panel', ['bg_color']],
-		['WindowDialog', 'panel', ['bg_color']]
+		['WindowDialog', 'panel', ['bg_color']],
+		['PopupMenu', 'panel', ['bg_color']],
+		['PopupDialog', 'panel', ['bg_color']],
 	],
 	'accent_color': [
 		['ProgressBar', 'fg', ['bg_color']],
 		['VSlider', 'slider', ['bg_color']],
-		['HSlider', 'slider', ['bg_color']]
+		['HSlider', 'slider', ['bg_color']],
+		['Button', 'focus', ['border_color']],
+		['LineEdit', 'focus', ['border_color']],
+		['TextEdit', 'focus', ['border_color']],
+		['CheckButton', 'focus', ['border_color']],
+		['CheckBox', 'focus', ['border_color']],
 	],
 	'card_bg_color': [
 		['PanelContainer', 'panel', ['bg_color']],
+		['PopupMenu', 'hover', ['bg_color']],
+		['Tree', 'selected', ['bg_color']],
+		['Tree', 'selected_focus', ['bg_color']],
 		['VSeparator', 'separator', ['color']],
-		['HSeparator', 'separator', ['color']]
+		['HSeparator', 'separator', ['color']],
+		['VScrollBar', 'grabber', ['bg_color']],
+		['VScrollBar', 'grabber_highlight', ['bg_color']],
+		['VScrollBar', 'grabber_pressed', ['bg_color']],
+		['HScrollBar', 'grabber', ['bg_color']],
+		['HScrollBar', 'grabber_highlight', ['bg_color']],
+		['HScrollBar', 'grabber_pressed', ['bg_color']],
 	],
 	'view_bg_color': [
 		['LineEdit', 'normal', ['bg_color']],
 		['TextEdit', 'normal', ['bg_color']],
 		['ProgressBar', 'bg', ['bg_color']]
 	]
+}
+const COLORS = {
+	'accent_color': {
+		'LineEdit': ['selection_color'],
+		'TextEdit': ['selection_color'],
+	},
+	'accent_fg_color': {
+		'LineEdit': ['font_color_selected'],
+		'TextEdit': ['font_color_selected'],
+	},
+	'window_fg_color': {
+		'Label': ['font_color'],
+		'RichTextLabel': ['default_color'],
+		'TabContainer': ['font_color'],
+		'WindowDialog': ['title_color'],
+		'PopupMenu': ['font_color'],
+		'CheckBox': ['font_color'],
+		'CheckButton': ['font_color'],
+		'Tree': ['font_color', 'font_color_selected'],
+		'FileDialog': ['file_icon_modulate', 'folder_icon_modulate'],
+	},
+	'card_fg_color': {
+		'PanelContainer': ['font_color'],
+	},
+	'view_fg_color': {
+		'LineEdit': ['font_color', 'cursor_color', 'clear_button_color'],
+		'Button': ['font_color'],
+		'TextEdit': ['font_color'],
+		'ProgressBar': ['font_color']
+	}
 }
 
 
@@ -48,15 +94,53 @@ func colorize_texture(type: String, img: String, col: Color):
 			if pix.s == 0:
 				continue
 
-			# keep value, change hue & sat
+			# blend
 			pix.h = col.h
 			pix.s = col.s * pix.s
+			pix.v = col.v * pix.v
 			data.set_pixel(x, y, pix)
 
 	data.unlock()
 	tex = ImageTexture.new()
 	tex.create_from_image(data)
 	theme.set_icon(img, type, tex)
+
+
+# sets multiple colors of type to value,
+# automatically creates colors for other potential states
+func set_colors(type: String, cols: Array, value: Color):
+	for c in cols:
+		for appendix in ['', '_fg', '_accel', '_separator']:
+			theme.set_color(c + appendix, type, value)
+
+		theme.set_color(
+			c + '_focus', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.1, 0.1))
+		)
+		theme.set_color(
+			c + '_bg', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.6, 0.1))
+		)
+		theme.set_color(
+			c + '_disabled', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.7, 0.1))
+		)
+		theme.set_color(
+			c + '_uneditable', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.7, 0.1))
+		)
+		theme.set_color(
+			c + '_readonly', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.7, 0.1))
+		)
+		theme.set_color(
+			c + '_hover', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.3, 0.1))
+		)
+		theme.set_color(
+			c + '_pressed', type,
+			Color(value.r, value.g, value.b, max(value.a - 0.5, 0.1))
+		)
 
 
 # sets multiple color props of type->box to value
@@ -75,37 +159,56 @@ func update_theme_colors():
 	var path = OS.get_environment('XDG_CONFIG_HOME') + '/gtk-3.0/gtk.css'
 	var ret_code = file.open(path, File.READ)
 	if ret_code != OK:
-		push_warning('Failed to read gtk.css: ERROR ' + str(ret_code))
+		push_warning(
+			'Failed to read gtk.css (' + str(ret_code)
+			+ '), keeping default colors'
+		)
 		return
 
 	# parse gtk.css
 	for line in file.get_as_text().split('\n'):
-		if not (line.begins_with('@define-color ') and 'rgb' in line):
+		if not (line.begins_with('@def') and ('rgb' in line or '#' in line)):
 			continue
 
-		# convert rgb(); or rgba(); to csv
-		var parts = line.replace('@define-color ', '').split(' rgb')
-		var key = parts[0]
-		var csv = parts[1].rstrip(');').lstrip('a(').replace(' ', '')
+		# prepare for color parsing
+		var color_value = Color.transparent
+		var key = ''
+		line = line.replace('@define-color ', '')
 
-		# get rgb(a) from csv
-		var values = csv.split(',')
-		var r = int(values[0])
-		var g = int(values[1])
-		var b = int(values[2])
-		var a = int(values[3]) if len(values) > 3 else 255
+		if 'rgb' in line:
+			# convert rgb(); or rgba(); to Color
+			var parts = line.split(' rgb')
+			key = parts[0]
+			var csv = parts[1].rstrip(');').lstrip('a(').replace(' ', '')
+			var values = csv.split(',')
+
+			color_value = Color8(
+				int(values[0]), int(values[1]), int(values[2]),
+				int(float(values[3]) * 255) if len(values) > 3 else 255
+			)
+		else:
+			# convert hex to Color
+			var parts = line.rstrip(';').split(' ')
+			key = parts[0]
+			color_value = Color(parts[-1])
 
 		# recolor images
 		if key == IMG_RECOLOR_KEY:
 			for type in IMAGES:
 				for img in IMAGES[type]:
-					colorize_texture(type, img, Color8(r, g, b, a))
+					colorize_texture(type, img, color_value)
+
+		# set font/misc colors
+		for c in COLORS:
+			if c == key:
+				for item in COLORS[c]:
+					set_colors(item, COLORS[c][item], color_value)
 
 		# recolor appropriate boxes
 		for c in BOX_PROPS:
 			if c == key:
 				for item in BOX_PROPS[c]:
-					set_box_props(item[0], item[1], item[2], Color8(r, g, b, a))
+					set_box_props(item[0], item[1], item[2], color_value)
 
 				break
 
